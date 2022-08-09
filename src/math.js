2fd759de86cb4059c926a0fb415031c7cc957caa
Andrey Anisimov <maddbuzz@gmail.com>
2022-08-09 17:14:29 +0800

    modified math.js

--- src/math.js
+++ src/math.js
@@ -2,7 +2,7 @@ export const randomInteger = (maximumExclusive) => Math.trunc(Math.random() * ma
 
 /* https://en.wikipedia.org/wiki/Euclidean_algorithm */
 export const greatestCommonDivisor = (nonZeroInteger1, nonZeroInteger2) => {
-  // if (nonZeroInteger1 === 0 || nonZeroInteger2 === 0) return null;
+  if (nonZeroInteger1 === 0 || nonZeroInteger2 === 0) return null;
   const remainder = nonZeroInteger1 % nonZeroInteger2;
   if (remainder === 0) return nonZeroInteger2;
   return greatestCommonDivisor(nonZeroInteger2, remainder);
