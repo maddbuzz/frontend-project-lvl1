ad544a54e07095de19022adbaddafa77c054e7e1
Andrey Anisimov <maddbuzz@gmail.com>
2022-08-10 18:44:55 +0800

    modify even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,12 +1,14 @@
 import runGame from '../index.js';
 import getRandomInteger from '../math.js';
 
+const isEven = (number) => number % 2 === 0;
+
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getGameQuestionAndAnswer = () => {
-  const question = getRandomInteger(100);
-  const answer = question % 2 === 0 ? 'yes' : 'no';
-  return [question, answer];
+  const maximumNumber = 100;
+  const question = getRandomInteger(maximumNumber);
+  return [question, isEven(question) ? 'yes' : 'no'];
 };
 
 export default () => runGame(gameDescription, getGameQuestionAndAnswer);
