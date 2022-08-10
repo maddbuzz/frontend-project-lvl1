3e5dfd78833e2c8729cacf3aa7a8c9c35619d838
Andrey Anisimov <maddbuzz@gmail.com>
2022-08-10 18:52:44 +0800

    modify prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -14,10 +14,9 @@ const isPrime = (number) => {
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getGameQuestionAndAnswer = () => {
-  const number = 2 + getRandomInteger(19);
-  const question = `${number}`;
-  const answer = isPrime(number) ? 'yes' : 'no';
-  return [question, answer];
+  const [minimumNumber, maximumNumber] = [2, 21];
+  const question = minimumNumber + getRandomInteger(maximumNumber - minimumNumber);
+  return [question, isPrime(question) ? 'yes' : 'no'];
 };
 
 export default () => runGame(gameDescription, getGameQuestionAndAnswer);
